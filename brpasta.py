440a132e31c42b36d91c1dfab7f8cc1635a19787
Eli Fachin Junior <juniorfachin@hotmail.com>
2019-09-26 23:20:09 -0300

    Commit inicial

---
+++ brpasta.py
@@ -0,0 +1,17 @@
+print("Bem vindo ao CopyPasta Finder Simulator 3001!"
+      + "\nAqui você escolhe a resposta pronta para as perguntas ou discussões chatas pakarai!")
+catg = ["0", "Drogas", "Política", "Jogos", "Foda-se"]
+selcat = int(input("Por favor, escolha sua categoria abaixo:"
+                   + "\n 1 - Drogas\n 2 - Política\n 3 - Jogos\n 4 - Foda-se\n"))
+
+if selcat == 0:
+    print("Oi zero")
+elif selcat != 0:
+    print("Muito bem! Você selecionou: " + catg[1])
+
+# Busca o copypasta do reddit
+# outra celula
+from six.moves import urllib
+
+def busca_copy_pasta
+  print('yoho')
