66226063a256e16a84a2040e31706f9369d3ccec
Eli Fachin Junior <juniorfachin@hotmail.com>
2019-09-26 23:49:38 -0300

    Adiciona praw.ini.example

--- brpasta.py
+++ brpasta.py
@@ -1,16 +1,22 @@
-print("Bem vindo ao CopyPasta Finder Simulator 3001!"
-      + "\nAqui você escolhe a resposta pronta para as perguntas ou discussões chatas pakarai!")
-catg = ["0", "Drogas", "Política", "Jogos", "Foda-se"]
-selcat = int(input("Por favor, escolha sua categoria abaixo:"
-                   + "\n 1 - Drogas\n 2 - Política\n 3 - Jogos\n 4 - Foda-se\n"))
+# print("Bem vindo ao CopyPasta Finder Simulator 3001!"
+#       + "\nAqui você escolhe a resposta pronta para as perguntas ou discussões chatas pakarai!")
+# catg = ["0", "Drogas", "Política", "Jogos", "Foda-se"]
+# selcat = int(input("Por favor, escolha sua categoria abaixo:"
+#                    + "\n 1 - Drogas\n 2 - Política\n 3 - Jogos\n 4 - Foda-se\n"))
 
-if selcat == 0:
-    print("Oi zero")
-elif selcat != 0:
-    print("Muito bem! Você selecionou: " + catg[1])
+# if selcat == 0:
+#     print("Oi zero")
+# elif selcat != 0:
+#     print("Muito bem! Você selecionou: " + catg[1])
 
 # Busca o copypasta do reddit
+import praw
 from six.moves import urllib
 
-def busca_copy_pasta
-  print('yoho')
+def busca_copy_pasta():
+  reddit = praw.Reddit()
+
+  for submission in reddit.subreddit('learnpython').hot(limit=10):
+      print(submission.title)
+
+busca_copy_pasta()
