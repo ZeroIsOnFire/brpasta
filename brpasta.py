e9e5a91b8b41b74733e460d2fe5d8e459fcb2f5b
gsatilio <g.satilio@gmail.com>
2019-09-27 18:39:22 -0300

    Update brpasta.py

--- brpasta.py
+++ brpasta.py
@@ -1,13 +1,57 @@
-# print("Bem vindo ao CopyPasta Finder Simulator 3001!"
-#       + "\nAqui você escolhe a resposta pronta para as perguntas ou discussões chatas pakarai!")
-# catg = ["0", "Drogas", "Política", "Jogos", "Foda-se"]
-# selcat = int(input("Por favor, escolha sua categoria abaixo:"
-#                    + "\n 1 - Drogas\n 2 - Política\n 3 - Jogos\n 4 - Foda-se\n"))
+"""
+### Inicio Idioma
+print("Choose your language: pt or en")
+var_idioma = input("Language: ")
+var_idiomack = False
+if var_idioma.lower() == "en":
+    var_idiomack = True
+    #print("English")
+elif var_idioma.lower() == "pt":
+    var_idiomack = True
+    #print("Português")
+elif var_idiomack == False:
+    print("This language is not valid")
+    exit()
 
-# if selcat == 0:
-#     print("Oi zero")
-# elif selcat != 0:
-#     print("Muito bem! Você selecionou: " + catg[1])
+print("Language chosen: " + var_idioma.lower())
+### Fim Idioma
+
+###Inicio Categoria
+var_categoria = input("Choose category: drugs, politics, memes, random, mock, compliment, menace, sorry: ")
+var_categoria_ck = False
+var_categoria_cod: int
+
+if var_categoria.lower() == "drugs":
+    var_categoria_ck = True
+    var_categoria_cod = 1
+elif var_categoria.lower() == "politics":
+    var_categoria_ck = True
+    var_categoria_cod = 2
+elif var_categoria.lower() == "memes":
+    var_categoria_ck = True
+    var_categoria_cod = 3
+elif var_categoria.lower() == "random":
+    var_categoria_ck = True
+    var_categoria_cod = 4
+elif var_categoria.lower() == "mock":
+    var_categoria_ck = True
+    var_categoria_cod = 5
+elif var_categoria.lower() == "compliment":
+    var_categoria_ck = True
+    var_categoria_cod = 6
+elif var_categoria.lower() == "menace":
+    var_categoria_ck = True
+    var_categoria_cod = 7
+elif var_categoria.lower() == "sorry":
+    var_categoria_ck = True
+    var_categoria_cod = 8
+elif var_categoria_ck == False:
+    print("This category does not exists")
+    exit()
+
+print("Category chosen: " + var_categoria.lower())
+### Fim Categoria
+"""
 
 # Busca o copypasta do reddit
 import praw
